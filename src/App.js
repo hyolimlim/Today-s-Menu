05a05e376f158b3318f87e2946bf42bf8e9701c8
hyolimLim <hyolimlim.dev@gmail.com>
2022-12-25 05:37:06 +0900

    fix: ErrorBoundary 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Notfound from "./pages/Notfound";
 import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
 import Header from "./components/Header";
+import Error from "./pages/Error";
 import "./styles/Design.scss";
 import React, { Suspense } from "react";
 import MainSkeleton from "./pages/Main/MainSkeleton";
 import RecipeProvider from "./store/RecipeProvider";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Main = React.lazy(() => import("./pages/Main/Main"));
 
@@ -16,14 +18,16 @@ function App() {
         <div className="warp">
           <Header />
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense fallback={<MainSkeleton />}>
-                  <Main />
-                </Suspense>
-              }
-            />
+            <ErrorBoundary fallback={<Error />}>
+              <Route
+                path="/"
+                element={
+                  <Suspense fallback={<MainSkeleton />}>
+                    <Main />
+                  </Suspense>
+                }
+              />
+            </ErrorBoundary>
             <Route path="/:id" element={<RecipeDetail />} />
             <Route element={<Notfound />} />
           </Routes>
