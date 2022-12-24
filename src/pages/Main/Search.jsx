0a0c66bf069e407833569eed9322aa858afc102d
hyolimLim <hyolimlim.dev@gmail.com>
2022-12-25 05:16:02 +0900

    fix: search 수정

--- src/pages/Main/Search.jsx
+++ src/pages/Main/Search.jsx
@@ -1,51 +1,43 @@
-import { useContext, useCallback, useState } from "react";
+import { useContext, useCallback, useEffect, useState, useRef } from "react";
 import { useGetData } from "../../hooks/useGetData";
 import { SearchIcon } from "../../assets/Index";
 import "../../styles/Design.scss";
 import { RecipeContext } from "../../store/RecipeProvider";
 
 function Search() {
-  const { setInput, setOption } = useContext(RecipeContext);
-  const [inputData, setInputData] = useState("");
-  const [optionData, setOptionData] = useState("음식명");
+  const { setInput, option, setOption } = useContext(RecipeContext);
+  const inputRef = useRef();
+  const optionRef = useRef();
 
   //엔터 누르면 검색 수정
   const onKeyDown = useCallback((e) => {
     if (e.key === "Enter" && e.target.value.trim().length > 0) {
-      setInputData(e.target.value.trim());
-      setInput(inputData);
-      setOption(optionData);
+      setInput(inputRef.current.value);
+      setOption(optionRef.current.value);
     }
   }, []);
-
-  const onClick = () => {
-    setInput(inputData);
-    setOption(optionData);
-  };
-
-  // const { Getquery } = useGetData();
 
   return (
     <div className="searchbar">
       <div className="searchbar__inputitem">
-        <select
-          onChange={(e) => {
-            setOptionData(e.target.value);
-          }}
-        >
+        <select ref={optionRef}>
           <option value="음식명">음식명</option>
           <option value="재료명">재료명</option>
         </select>
         <input
           className="input"
           onKeyDown={onKeyDown}
-          onChange={(e) => {
-            setInputData(e.target.value);
-          }}
+          ref={inputRef}
           placeholder="레시피를 검색해보세요"
         ></input>
       </div>
-      <div className="searchbar__icon" onClick={onClick}>
+      <div
+        className="searchbar__icon"
+        onClick={() => {
+          setInput(inputRef.current.value);
+          setOption(optionRef.current.value);
+        }}
+      >
         <SearchIcon width="30px" height="30px" fill="#5f8d4e" />
       </div>
     </div>
